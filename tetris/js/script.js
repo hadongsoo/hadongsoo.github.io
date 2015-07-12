6809add672a546f17420ad3e30583ca716562592
dongsoo <keass@naver.com>
2015-07-12 23:51:18 +0900

    기본 골자

--- tetris/js/script.js
+++ tetris/js/script.js
@@ -1,1 +1,23 @@
-console.log('asdf');
+"use strict";
+
+/*
+* 기본 스펙
+* 영역 만들기 : 스테이지, 다음블럭, 점수 노출
+* 컨트롤 : 스페이스, 상하좌우,
+* 점수 데이터 : 점수
+* 블럭 정보 : 7개 영역
+* 반복 : 초기화, 게임 루핑, 속도
+*
+* */
+
+var tetris = tetris || {};
+
+tetris.looping = function(){
+    console.log(this);
+};
+
+tetris.looping.prototype = {
+    loop:function(){
+        console.log('asfdasdf');
+    }
+};
