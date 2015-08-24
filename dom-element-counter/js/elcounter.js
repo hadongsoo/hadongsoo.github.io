64e3bcea57f6a0a785b714edb73727df8b02fa6c
dongsoo <ha.dongsoo@nhn.com>
2015-08-24 18:10:39 +0900

    북마클릿 버전1

--- dom-element-counter/js/elcounter.js
+++ dom-element-counter/js/elcounter.js
@@ -1,106 +1,115 @@
-"use strict";
-/*
-* 현재 찝힌 영역에만 작동 li>a a 찍으면 li들 검색 안됌. 상위로 올라가며 시빌링 있는 존재만 카운터
-* 찍은 요소 말고, 인접한 형제들 알아내서 표시해주기?
-* 배경색 일시적으로 추가하는 부분에서, 형제들 문제있어서 부모 배경색 지정
-* */
 
-var elCounter = function(){
-    var that = this;
+(function() {
+    "use strict";
 
-    var now = 0;
-    var all = 0;
+    var cssLoader = function(){
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'http://localhost:9000/www/css/style.css';
+        link.media = 'all';
+        console.log(link);
+        head.appendChild(link);
+    };
+    cssLoader();
 
-    document.addEventListener('mouseover',function(e){
-        var target = e.target;
+    var elCounter = function () {
+        var that = this;
 
-        var tagSibilingArry = [];
-        var tagSibilingArryCount=0;
+        var now = 0;
+        var all = 0;
+
+        document.addEventListener('mouseover', function (e) {
+            var target = e.target;
+
+            var tagSibilingArry = [];
+            var tagSibilingArryCount = 0;
 
 
-        if (e.target.parentNode.childNodes.length>1  ) { //같은 자식이 2개 이상
-            for (var i =0; i<e.target.parentNode.childNodes.length; i++ ){ //같은자식간에 탐색
-                if (e.target.parentNode.childNodes[i].tagName && e.target.parentNode.tagName){ //태그 인지 검증, 부모
+            if (e.target.parentNode.childNodes.length > 1) { //같은 자식이 2개 이상
+                for (var i = 0; i < e.target.parentNode.childNodes.length; i++) { //같은자식간에 탐색
+                    if (e.target.parentNode.childNodes[i].tagName && e.target.parentNode.tagName) { //태그 인지 검증, 부모
 
-                    tagSibilingArry[tagSibilingArryCount] = e.target.parentNode.childNodes[i];
+                        tagSibilingArry[tagSibilingArryCount] = e.target.parentNode.childNodes[i];
 
-                    if (e.target == tagSibilingArry[tagSibilingArryCount]){
-                        now = tagSibilingArryCount+1;
-                        //console.log(e.target.parentNode.childNodes[i]);
-                        bgChanger(e.target.parentNode,'pointyoufather');
-                        bgChanger(e.target.parentNode.childNodes[i],'pointyou');
+                        if (e.target == tagSibilingArry[tagSibilingArryCount]) {
+                            now = tagSibilingArryCount + 1;
+                            //console.log(e.target.parentNode.childNodes[i]);
+                            bgChanger(e.target.parentNode, 'pointyoufather');
+                            bgChanger(e.target.parentNode.childNodes[i], 'pointyou');
+                        }
+                        tagSibilingArryCount = tagSibilingArryCount + 1;
                     }
-                    tagSibilingArryCount=tagSibilingArryCount+1;
                 }
+                all = tagSibilingArry.length;
             }
-            all = tagSibilingArry.length;
-        }
 
-        haveData('.blob',now,all);
-        makeMove('.blob');
-        bgRemover('pointyou');
-        bgRemover('pointyoufather');
+            haveData('.blob', now, all);
+            makeMove('.blob');
+            bgRemover('pointyou');
+            bgRemover('pointyoufather');
 
-    });
-    var makeElement = function(what,classname){
-        if (!document.querySelector("."+classname)){
-            var blobFrag = document.createDocumentFragment();
-            blobFrag = document.createElement(what);
-            document.body.appendChild(blobFrag);
-            blobFrag.classList.add(classname);
-            blobFrag.classList.add('hide');
-        }
-    };
-    var makeMove = function(what){
-        var x = 0,
-            y = 0,
-            xCali = 45,
-            yCali = 25;
-        document.addEventListener('mousemove',function(e){
-            x = e.clientX-yCali;
-            y = e.clientY-xCali;
-
-            var blob = document.querySelector(what);
-            var data = 'top:'+y+'px; left:'+x+'px; position: absolute;';
-            blob.setAttribute('style',data);
         });
-    };
-    var haveData = function(what,now,all){
-        var blob = document.querySelector(what);
-        if (all>1) {
-            blob.classList.remove('hide');
-            blob.innerHTML = now+'/'+all;
-        } else {
-            blob.classList.add('hide');
-        }
+        var makeElement = function (what, classname) {
+            if (!document.querySelector("." + classname)) {
+                var blobFrag = document.createDocumentFragment();
+                blobFrag = document.createElement(what);
+                document.body.appendChild(blobFrag);
+                blobFrag.classList.add(classname);
+                blobFrag.classList.add('hide');
+            }
+        };
+        var makeMove = function (what) {
+            var x = 0,
+                y = 0,
+                xCali = 45,
+                yCali = 25;
+            document.addEventListener('mousemove', function (e) {
+                x = e.pageX - yCali;
+                y = e.pageY - xCali;
+
+                var blob = document.querySelector(what);
+                var data = 'top:' + y + 'px; left:' + x + 'px; position: absolute;';
+                blob.setAttribute('style', data);
+            });
+        };
+        var haveData = function (what, now, all) {
+            var blob = document.querySelector(what);
+            if (all > 1) {
+                blob.classList.remove('hide');
+                blob.innerHTML = now + '/' + all;
+            } else {
+                blob.classList.add('hide');
+            }
+
+        };
+        var bgChanger = function (element, classname) {
+            if (element.tagName !== 'HTML' &&
+                element.tagName !== 'BODY' &&
+                element.tagName !== 'HEAD') {
+                element.classList.add(classname);
+            }
+        };
+        var bgRemover = function (classname) {
+            var uncover = document.querySelectorAll(classname);
+            if (uncover.classList.contains(classname)) {
+                uncover.classList.remove(classname);
+            }
+        };
+        makeElement('div', 'blob');
+
 
     };
-    var bgChanger = function(element, classname){
-        if (element.tagName !== 'HTML' &&
-            element.tagName !== 'BODY' &&
-            element.tagName !== 'HEAD'){
-            element.classList.add(classname);
-        }
-    };
-    var bgRemover = function(classname){
-        var uncover = document.querySelectorAll(classname);
-        if (uncover.classList.contains(classname)){
-            uncover.classList.remove(classname);
-        }
-    };
-    makeElement('div','blob');
+
+    var test = new elCounter();
+})();
 
 
+//javascript:var h=document.createElement('link');h.setAttribute('href','http://localhost:9000/www/css/style.css'); var e=document.createElement('script');e.setAttribute('language','javascript');e.setAttribute('src','http://localhost:9000/js/elcounter.js');document.head.appendChild(h);document.body.appendChild(e);void(0);
 
-
-
-
-};
-
-//    elCounter.prototype = {
-//};
-
-var test = new elCounter();
-
-
-
+/*
+* 개선할 문제. CSS없이 js내에서 스타일 다 주기. 백그라운드만 남기기
+* 오브젝트 태그 이름도 같이 보이기
+* 북마클릿 종료 기능
+* */
---
+++ dom-element-counter/www/js/elcounter.js
@@ -0,0 +1,101 @@
+"use strict";
+/*
+* 현재 찝힌 영역에만 작동 li>a a 찍으면 li들 검색 안됌. 상위로 올라가며 시빌링 있는 존재만 카운터
+* 찍은 요소 말고, 인접한 형제들 알아내서 표시해주기?
+* 배경색 일시적으로 추가하는 부분에서, 형제들 문제있어서 부모 배경색 지정
+* */
+
+var elCounter = function(){
+    var that = this;
+
+    var now = 0;
+    var all = 0;
+
+    document.addEventListener('mouseover',function(e){
+
+        var target = e.target;
+        var tagSibilingArry = [];
+        var tagSibilingArryCount=0;
+
+        if (e.target.parentNode.childNodes.length>1  ) { //같은 자식이 2개 이상
+            for (var i =0; i<e.target.parentNode.childNodes.length; i++ ){ //같은자식간에 탐색
+                if (e.target.parentNode.childNodes[i].tagName && e.target.parentNode.tagName){ //태그 인지 검증, 부모
+
+                    tagSibilingArry[tagSibilingArryCount] = e.target.parentNode.childNodes[i];
+
+                    if (e.target == tagSibilingArry[tagSibilingArryCount]){
+                        now = tagSibilingArryCount+1;
+                        //console.log(e.target.parentNode.childNodes[i]);
+                        bgChanger(e.target.parentNode,'pointyoufather');
+                        bgChanger(e.target.parentNode.childNodes[i],'pointyou');
+                    }
+                    tagSibilingArryCount=tagSibilingArryCount+1;
+                }
+            }
+            all = tagSibilingArry.length;
+        }
+        haveData('.blob',now,all);
+        makeMove('.blob');
+        bgRemover('pointyou');
+        bgRemover('pointyoufather');
+
+    });
+    var makeElement = function(what,classname){
+        var blobFrag = document.createDocumentFragment();
+
+        blobFrag = document.createElement(what);
+        document.body.appendChild(blobFrag);
+        blobFrag.classList.add(classname);
+    };
+    var makeMove = function(what){
+        var x = 0,
+            y = 0,
+            xCali = 45,
+            yCali = 25;
+        document.addEventListener('mousemove',function(e){
+            x = e.clientX-yCali;
+            y = e.clientY-xCali;
+
+            var blob = document.querySelector(what);
+            var data = 'top:'+y+'px; left:'+x+'px; position: absolute;';
+            blob.setAttribute('style',data);
+        });
+    };
+    var haveData = function(what,now,all){
+        var blob = document.querySelector(what);
+        if (all>1) {
+            blob.classList.remove('hide');
+            blob.innerHTML = now+'/'+all;
+        } else {
+            blob.classList.add('hide');
+        }
+    };
+    var bgChanger = function(element, classname){
+        if (element.tagName !== 'HTML' &&
+            element.tagName !== 'BODY' &&
+            element.tagName !== 'HEAD'){
+            element.classList.add(classname);
+        }
+    };
+    var bgRemover = function(classname){
+        var uncover = document.querySelectorAll(classname);
+        if (uncover.classList.contains(classname)){
+            uncover.classList.remove(classname);
+        }
+    };
+    makeElement('div','blob');
+
+
+
+
+
+
+};
+
+    elCounter.prototype = {
+};
+
+var test = new elCounter();
+
+
+
